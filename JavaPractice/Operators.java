5a6d169bb3e5f59981eb5be5c50b078811b0f71f
bijay-odyssey <bijaybeezoe@gmail.com>
2025-06-30 17:54:25 +0545

    operators

---
+++ JavaPractice/Operators.java
@@ -0,0 +1,19 @@
+public class Operators {
+    public static void main(String[] args){
+        int a = 10, b = 5;
+        int sum = a+b;
+        int product = a*b;
+        boolean isEqual = a==b;
+        boolean result = (a>b) && (b>0);
+
+        System.out.println(a);
+        System.out.println(b);
+        System.out.println(sum);
+        System.out.println(product);
+        System.out.println(isEqual);
+        System.out.println(result);
+
+
+
+    }
+}

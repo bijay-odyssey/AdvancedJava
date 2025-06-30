844e16f46017f1ffb861da7c33663641c62be84e
bijay-odyssey <bijaybeezoe@gmail.com>
2025-06-30 17:54:53 +0545

    cf

---
+++ JavaPractice/ControlFlow.java
@@ -0,0 +1,16 @@
+public class ControlFlow {
+    public static void main(String[] args){
+        int score = 85;
+
+        if(score >=90){
+            System.out.println("Grade A");
+
+        }
+        else if (score >= 75){
+            System.out.println("Grade B");
+        }
+        else{
+            System.out.println("Grade C or Below");
+        }
+    }
+}

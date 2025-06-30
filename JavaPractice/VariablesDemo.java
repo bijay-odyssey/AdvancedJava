b324c414beaf2e6e351d9d5d5bfd0482b29ba70e
bijay-odyssey <bijaybeezoe@gmail.com>
2025-06-30 17:54:05 +0545

    variables

---
+++ JavaPractice/VariablesDemo.java
@@ -0,0 +1,18 @@
+public class VariablesDemo {
+    public static void main(String[] args){
+     int age = 5;
+     double pi = 3.14;
+     char grade = 'A';
+     boolean isJava = true;
+     String name = "Buzz";
+
+     System.out.println("Age: "+age);
+     System.out.println("Pi "+pi);
+     System.out.println("Grade "+grade);
+     System.out.println("Is it true: "+isJava);
+     System.out.println("your name: "+name);
+
+
+
+    }
+}

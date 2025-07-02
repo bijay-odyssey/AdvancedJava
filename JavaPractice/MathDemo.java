83c08d4f84a2bba8c53b109284c57e0d1896314e
bijay-odyssey <bijaybeezoe@gmail.com>
2025-07-02 17:25:31 +0545

    mathdemo

---
+++ JavaPractice/MathDemo.java
@@ -0,0 +1,14 @@
+public class MathDemo {
+    public static void main(String[] args){
+        System.out.println("Max: "+ Math.max(10,20));
+        System.out.println("Max: "+ Math.min(10,20));
+        System.out.println("Sqrt: "+ Math.sqrt(100));
+        System.out.println("Power: "+ Math.pow(10,2));
+        System.out.println("Abs: "+ Math.abs(-20));
+        System.out.println("Pi: "+ Math.PI);
+        System.out.println("Random(0-1): "+ Math.random());
+
+
+
+    }
+}
